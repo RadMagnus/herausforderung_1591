228cba0295ae803cdefff62c26fbd878d797517f
Samuel Klein <it@samuel-klein.net>
2020-03-21 11:52:15 +0100

    expanded infected and deceased cases to dedicated rows and fixed date

--- get-basedata-rki.py
+++ get-basedata-rki.py
@@ -34,6 +34,11 @@ def get_rki():
 """
 returns the data with a datapoint for each reported case with 
 timestamp (useful for timeseries)
+
+an entry can contain multiple reported cases which are separated into 
+different rows where infected and deceased cases are further divided.
+the date contains information about the time but only the day is used 
+so this column is cutted to only contain YYYY-MM-DD.
 """
 def get_rki_with_timestamp():
     url = "https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.csv"
@@ -42,9 +47,38 @@ def get_rki_with_timestamp():
     if not response or not response.status_code == 200:
         print("could not fetch api")
         sys.exit(1)
-    response = response.text
+    response = response.text.split("\n")
+
+    # removes the columns for the number of infected, deceased cases and adds
+    # a new header for the status
+    header = response[0].split(",")
+    del header[5]
+    del header[5]
+    header.append("status")
+    header = ",".join(header)
+
+    new_response = [header]
     
-    return pd.read_csv(StringIO(response), sep=",")
+    for row in response[1:-1]:
+        r_arr = row.split(",")
+
+        # removes the columns for the number of infected, deceased cases and adds
+        # a new header for the status
+        num_cases = int(r_arr[5])
+        num_dead = int(r_arr[6])
+        r_arr[8] = r_arr[8][:10]
+
+        del r_arr[5]
+        del r_arr[5]
+
+        for i in range(num_cases):
+            new_response.append(",".join(r_arr+["infected"]))
+        for i in range(num_dead):
+            new_response.append(",".join(r_arr+["deceased"]))
+
+    new_response = "\n".join(new_response)
+    
+    return pd.read_csv(StringIO(new_response), sep=",")
 
 df = get_rki_with_timestamp()
 
