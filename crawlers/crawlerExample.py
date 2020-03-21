c8fbb9f0879749547f5d1a86acdfdf00ad2c6a0c
Robert Hülsmann <robert@huelsi.de>
2020-03-21 15:12:17 +0100

    crawler class structure added

---
+++ crawlers/crawlerExample.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+class CrawlerExample: # TODO: Specify the class name in relation to the source or content of this dataset
+    """
+    Example class for a Child Crawler  # TODO: Change the description of this crawler
+    """
+
+    name = 'Example Crawler'  # TODO: Specify the name
+
+    def __init__(self):
+        pass
+
+    def crawl(self):
+        """
+        Get data from web service and save it as CSV (semicolon separated) in data/raw
+        :return: true if done w/o errors
+        """
+        # TODO: Write the crawl job here
+        print('crawl - crawl - crawl')
+        return True
