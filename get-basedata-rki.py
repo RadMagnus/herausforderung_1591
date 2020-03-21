53ae9ef709cb840f82f1181089274d75f2635ab5
Samuel Klein <it@samuel-klein.net>
2020-03-21 02:22:17 +0100

    added better api with timestamps for timeseries

--- get-basedata-rki.py
+++ get-basedata-rki.py
@@ -8,6 +8,7 @@ __author__ = "Samuel Klein"
 
 import sys, os
 import time
+from io import StringIO
 import requests
 import pandas as pd
 
@@ -15,6 +16,9 @@ if len(sys.argv) < 2:
     print("no output directory given: script.py <path>")
     sys.exit()
 
+"""
+returns the basic numbers of reported cases
+"""
 def get_rki():
     url = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=1%3D1&outFields=GEN,BEZ,EWZ,cases,deaths,BL,BL_ID,county,RS&returnGeometry=false&outSR=4326&f=json"
     response = requests.get(url)
@@ -25,10 +29,24 @@ def get_rki():
     response = response.json()["features"]
     
     cases = [c["attributes"] for c in response]
-    df = pd.DataFrame(data=cases)
-    return df
+    return pd.DataFrame(data=cases)
 
-df = get_rki()
+"""
+returns the data with a datapoint for each reported case with 
+timestamp (useful for timeseries)
+"""
+def get_rki_with_timestamp():
+    url = "https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.csv"
+    response = requests.get(url)
+    response.encoding = "utf-8"
+    if not response or not response.status_code == 200:
+        print("could not fetch api")
+        sys.exit(1)
+    response = response.text
+    
+    return pd.read_csv(StringIO(response), sep=",")
+
+df = get_rki_with_timestamp()
 
 ts = str(int(time.time()))
 outfile = os.path.join(sys.argv[1], "rki-cases_"+ts+".tsv")
